3a22f360fa03a66e12402be09668e7b072f8baf3
Pankaj Nikumbh <nikumbhpankaj@170gmail.com>
2022-08-25 10:16:57 +0530

    Conflict project addd

---
+++ GitConflit/src/main/java/com/BikkadIT/GitConflitApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConflitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConflitApplication.class, args);
+	}
+
+}
